3a909b7fdd5bdfba783ba1fcdcc142f2618967cf
amira <amira.a.a.hashim@gmail.com>
2020-03-18 17:52:26 +0200

    trying rest_framework

---
+++ note/note_api.py
@@ -0,0 +1,13 @@
+#like form but for serializing data
+from rest_framework import serializers , viewsets
+#Serializers are used to convert data from different formats like JSON, XMl to complex data types like querysets
+from .models import Note
+
+class NoteSerialiser(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model=Note
+        fields = ('id', 'title', 'description', 'created_at', 'created_by', 'priority')
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class =NoteSerialiser
